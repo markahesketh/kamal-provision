35cbc7627a955d923590ec6891ffb858337c1527
Mark Hesketh <contact@markhesketh.co.uk>
2025-12-24 19:44:03 +0000

    chore: generate gem

---
+++ lib/kamal/provision.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "provision/version"
+
+module Kamal
+  module Provision
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
